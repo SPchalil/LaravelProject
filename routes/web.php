671d007cbbd9ca3df4c05522b0f45110673dacff
Swapna Chalil <74065235+SPchalil@users.noreply.github.com>
2021-08-04 16:37:11 -0230

    PagesController

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+/*
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::get('/users/{id}/{name}', function ($id, $name) {
+    return 'This is user ' .$name. 'with an id of '.$id;
+});
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
+*/
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
