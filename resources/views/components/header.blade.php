f7d474aa423e1fc6be8719174031cd305fcc339e
Swapna Chalil <74065235+SPchalil@users.noreply.github.com>
2021-09-28 17:02:50 -0230

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="header"> 
-        <h1 > Swapna <strong>ArtGallery </strong></h1>
+        <h1 > SWAPNA <strong>ArtGallery </strong></h1>
         <div class="header-right"> 
             <h3> Login/Register </h3>
             <h3> Cart </h3>
@@ -12,7 +12,7 @@
         <h3 > HOME </h3>
         <h3 > ABOUT ME </h3>
         <h3 > WATERCOLOR </h3>
-        <h3 > OILPAINTINGS </h3>
+        <h3 > OIL </h3>
         <h3 > ACRYLIC </h3>
         <h3 > MURAL </h3>
         <h3 > BOTTLEART </h3>
